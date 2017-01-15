7214b73bb1790189e338c79a02af3c934586103a
ljdmclean <lonitamclean2017@u.northwestern.edu>
2017-01-15 15:26:10 -0600

    Added code to read in data files

--- EDA.R
+++ EDA.R
@@ -1,11 +1,20 @@
-mobile.df <- read.csv("Merged_Data.csv", header = TRUE)
-head(mobile.df)
-str(mobile.df$category)
 
-app_events.df <- read.csv("C:\Users\lonit\OneDrive\Documents\498\Original_Data\app_events.csv")
+#Read in gender and age - includes device id, gender, age, and group
+gender_age.df <- read.csv("C:/Users/lonit/OneDrive/Documents/498/Original_Data/gender_age.csv")
 
+#Read in events - includes event id, timestamp, longitude, and latitude
+events.df <- read.csv("C:/Users/lonit/OneDrive/Documents/498/Original_Data/events.csv")
+
+#Read in phone device model - includes event id, timestamp, longitude, and latitude
+phone_device_model.df <- read.csv("C:/Users/lonit/OneDrive/Documents/498/Original_Data/phone_brand_device_model.csv")
+
+#Read in app events data - includes event id, app id, is installed and is active
 app_events.df <- read.csv("C:/Users/lonit/OneDrive/Documents/498/Original_Data/app_events.csv")
 
+#Read in app labels - includes app id and label id
+app_labels.df <- read.csv("C:/Users/lonit/OneDrive/Documents/498/Original_Data/app_labels.csv")
+
+#Read in label categories - includes label_id and category
 label_categories.df <- read.csv("C:/Users/lonit/OneDrive/Documents/498/Original_Data/label_categories.csv")
 
 summary(app_events.df$event_id)
